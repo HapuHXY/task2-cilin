db5b99bb522b857260b425ebdb380bf0eaed1f9b
HapuHXY <39945628+HapuHXY@users.noreply.github.com>
2020-04-26 19:35:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,64 +6,83 @@
 
 """
 code_word  以编码为key，单词list为value的dict，一个编码有多个单词
-word_code  以单词为key，编码为value的dict，一个单词可能有多个编码
 """
 code_word={}
-word_code={}
 cilin_file='/home/hxy/task2-cilin/new_cilin.txt'
 res_file='/home/hxy/task2-cilin/result.txt'
+word_second_no={"Aa","Ac","Ad","Ae","Af","Ag","Ai","Aj","Ak","Al","Am","Ba","Bb","Bc"}
 
+pairs=[]
 
 with open(cilin_file,'r',encoding='gbk') as f:
 	for line in f.readlines():
 		res=line.split()
-		code=res[0]
+		
+		#先抽取第一大类至第四大类的名词词语
+		if res[0][0] >='E':
+			continue
+
+		code=res[0]	
 		words=res[1:]
 		code_word[code]=words
-
-		for word in words:
-			if word in word_code.keys():
-				word_code[word].append(code)
-			else:
-				word_code[word]=[code]
 		
-				
-		#通过原子词群的编码，得到大，中，小类的编码,大类，中类的词具有很强的概括性，一般作为上位词
+		if len(code)>6 and res[0][0]>'E':
+			break
+
+		#通过原子词群的编码，得到大，中，小类的编码
 		if len(code)<6:	
-			continue
-		else:		
-			#上位词集合
-			word_fu_set=set()
-			word_fu_set.update(code_word[code[:1]],code_word[code[:2]],[code_word[code[:5]][0]])
-			#词段/原子词群中的词作为下位词，一般词段中第一个词是小类中的词，可以作为整个词群的上位词
-			
-			word_zi_set=set()
-			word_zi_set.update(words)
-			
-			with open(res_file,'a') as o:
-				#从原子词群向上找上层词，依次作为下层词的上位词
-				for word_zi in word_zi_set:
-					for word_fu in word_fu_set:
-						if word_zi == word_fu:
-							continue
-						o.write(word_zi)
-						o.write('\t')
-						o.write(word_fu)
-						o.write('\n')	
+			continue		
 	
-
-
-
+		#大类词
+		word_first=code_word[code[:1]][0]
+		#中类词
+		word_second=code_word[code[:2]][0]
+		#小类词
+		word_third_set=set()
+		word_third_set.update(code_word[code[:4]])
+		#原子词群，对于原子词群中的第一个词段，若整个词段为同义词，由于这些词意思相同，且多存在不常用词，只保留其第一个词语;
+		word_last_set=set()
+		if(code[-2]=="1" and code[-1]=="="):
+			word_last_set.add(code_word[code][0])
+		else:
+			word_last_set.update(code_word[code])
+		#词群中的第一个词为整个词群的标题词，可以作为词段的上位词
+		word_last_fu=code_word[code[:5]][0]
+	
+		
+		#若中类词是非描述性的词语，一般可以作为大类词的下位词;此时可抽取到<中类词，大类词>,<小类词，中类词>上下位关系词对
+		if code[:2] not in word_second_no:
+			s_f=[word_second,word_first]
+			if s_f not in pairs:
+				pairs.append(s_f)
+			for word_third in word_third_set: 		
+				if word_second == word_third:
+					continue
+				t_s=[word_third,word_second]
+				if t_s not in pairs:
+					pairs.append(t_s)
+		else:#否则，跳过中类词，抽取出<小类词，大类词>上下位关系词对
+			for word_third in word_third_set:
+				t_f=[word_third,word_first]
+				if t_f not in pairs:
+					pairs.append(t_f)
+		#<词群词，标题词>
+		for word_zi in word_last_set:
+			if word_zi == word_last_fu:
+				continue
+			z_f=[word_zi,word_last_fu]
+			if z_f not in pairs:
+				pairs.append(z_f)
 				
+
+with open(res_file,'a') as o:
+	for pair in pairs:
+		o.write(pair[0])
+		o.write('\t')
+		o.write(pair[1])
+		o.write('\n')
+
 	
-
-
-
-
-
-
-
-
 
 
 
